7bd22987424b7ee195250f3ab102e488801795e7
Dan McGee <dpmcgee@gmail.com>
2015-11-09 04:26:13 +0000

    Add inital draft of sniff program

--- Makefile
+++ Makefile
@@ -2,10 +2,13 @@ CPPFLAGS=-D_FORTIFY_SOURCE=2
 CFLAGS=-O2 -pipe -fstack-protector --param=ssp-buffer-size=4 -Wall
 LDFLAGS=-Wl,-O1,--sort-common,--as-needed,-z,relro
 
-all: rfoutlet
+all: rfoutlet sniff
 
 rfoutlet: rfoutlet.o
 	$(CC) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS) $+ -o $@ -lwiringPi
 
+sniff: sniff.o
+	$(CC) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS) $+ -o $@ -lwiringPi
+
 clean:
-	$(RM) *.o rfoutlet
+	$(RM) *.o rfoutlet sniff
---
+++ sniff.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <wiringPi.h>
+
+static const int pulse_length = 186;
+static const float tolerance = 0.10;
+
+// http://wiringpi.com/wp-content/uploads/2013/03/pins.pdf
+static const int pin = 2;
+
+static void handle_interrupt();
+
+int main(int argc, char *argv[])
+{
+	wiringPiSetup();
+	pinMode(pin, INPUT);
+
+    wiringPiISR(pin, INT_EDGE_BOTH, &handle_interrupt);
+
+	sleep(10);
+}
+
+static int switch_number(int code)
+{
+	int tranform = (code >> 4) & 0x3ff;
+	if ((tranform & 0x3) == 0x3) return 1;
+	if ((tranform & 0xc) == 0xc) return 2;
+	if ((tranform & 0x30) == 0x30) return 3;
+	if ((tranform & 0xc0) == 0xc0) return 4;
+	if ((tranform & 0x300) == 0x300) return 5;
+	return -1;
+}
+
+static void handle_interrupt()
+{
+	static unsigned int last_time;
+	static unsigned int first_duration;
+	static int code;
+	static int processing;
+	
+	unsigned int time = micros();
+	unsigned int duration = time - last_time;
+
+	if (duration > 30 * pulse_length) {
+		if (code != 0) {
+			printf("raw code: %d\n", code);
+			printf("on/off: %s\n", (code & 0xf) == 0x3 ? "on" : "off");
+			printf("switch: %d\n", switch_number(code));
+			printf("group: 0x%x\n", code >> 16); 
+		}
+		code = 0;
+		processing = 1;
+	} else if (processing) {
+		if ((duration > pulse_length * 0.67 && duration < pulse_length * 1.5) ||
+				(duration > pulse_length * 2.67 && duration < pulse_length * 3.5)) {
+			if (first_duration == 0) {
+				first_duration = duration;
+			} else {
+				// we have a full bit, so shift what we've collected so far to the left
+				code <<= 1;
+				if (first_duration > duration) {
+					// this is a one, longer pulse was first
+					code += 1;
+				}
+				first_duration = 0;
+			}
+		} else {
+			code = 0;
+			processing = 0;
+			first_duration = 0;
+		}
+	}
+
+	last_time = time;
+}
+
